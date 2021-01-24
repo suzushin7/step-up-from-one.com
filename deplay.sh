a63e3f0b68b718e4bd0184cac4c3e67cc58eca39
Shingo Suzuki <suzushinlab@gmail.com>
2021-01-24 13:46:57 +0900

    [update] blog

---
+++ deplay.sh
@@ -0,0 +1,9 @@
+host="www143.conoha.ne.jp"
+port="8022"
+user="c6595488"
+target="/home/${user}/public_html/step-up-from-one.com/"
+
+git add --all
+git commit -m "[update] blog"
+git push origin master
+rsync -p ${port} -e "ssh -i ~/.ssh/conohawing.pem" ${user}@${host}:${target}
