533c7b784610a9f6be16c35fc7fd12508b54189f
Shingo Suzuki <suzushinlab@gmail.com>
2021-01-24 13:49:47 +0900

    [update] blog

--- deplay.sh
+++ deplay.sh
@@ -5,5 +5,5 @@ target="/home/${user}/public_html/step-up-from-one.com/"
 
 git add --all
 git commit -m "[update] blog"
-git push origin master
+git push origin main
 rsync -p ${port} -e "ssh -i ~/.ssh/conohawing.pem" ${user}@${host}:${target}
